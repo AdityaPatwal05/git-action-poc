a9b78ca6dd817774a75e00ca2570efcc745f0bae
Aditya Patwal <adityapatwal.152@gmail.com>
2025-02-08 13:26:46 +0530

    added resources

--- Translation/Program.cs
+++ Translation/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
 using Translation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddLocalization();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var host = builder.Build();
+var js = host.Services.GetRequiredService<IJSRuntime>();
+await SetLanguage(js, "en-US");
+await host.RunAsync();
 
-await builder.Build().RunAsync();
+async Task SetLanguage(IJSRuntime jsRuntime, string culture)
+{
+    var newCulture = new CultureInfo(culture);
+    CultureInfo.DefaultThreadCurrentCulture = newCulture;
+    CultureInfo.DefaultThreadCurrentUICulture = newCulture;
+
+    //await jsRuntime.InvokeVoidAsync("blazorCulture.set", culture);
+}
